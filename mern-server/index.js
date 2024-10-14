6744879e01647963f5e264c2d8e3d54c641da0b2
tharuNethuu <tharu0nethu@gmail.com>
2024-10-14 21:03:46 +0530

    reupdate url

--- mern-server/index.js
+++ mern-server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 //middleware
 app.use(cors({
-  origin: ['https://nethbookpoint-front.onrender.com'],
+  origin: ['http://localhost:5173/'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
